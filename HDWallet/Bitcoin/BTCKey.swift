fd8c96343ac329b3a2eb31b5896474394e2fed4e
Brett Doffing <doffing81@bretts-macbook-pro.local>
2019-03-08 19:31:10 -0600

    Refactored BTCKey to deal with Data instead of Strings.

--- HDWallet/Bitcoin/BTCKey.swift
+++ HDWallet/Bitcoin/BTCKey.swift
@@ -4,54 +4,43 @@ import Foundation
 
 class BTCKey {
     
-    var privateKey: String?
-    var publicKey: String?
+    var privateKey: Data?
+    var publicKey: Data?
     var address: String?
     
-    init(withPrivateKey prvkey: String, testnet: Bool = false) {
-        privateKey = prvkey
-        let pubkey = BTCCurve.shared.pubkeyForHexPrivateKey(prvkey)
-        publicKey = pubkey
-        let hashedPk = hash160(pubkey: pubkey)
-        let modifiedPubKey = concatenateAddress(hashedPublicKey: hashedPk, testnet: testnet)
-        address = modifiedPubKey.base58CheckEncodeHexString()
+    init(withPrivateKey prvkey: Data, network: BTCNetwork = .main) {
+        self.privateKey = prvkey
+        let pubkey = BTCCurve.shared.generatePublicKey(privateKey: prvkey)
+        self.publicKey = pubkey
+        let hashedPk = hash160(pubkey: pubkey!)
+        let modifiedPubKey = concatenateAddress(hashedPublicKey: hashedPk, network: network)
+        self.address = modifiedPubKey.toHexString().base58CheckEncodeHexString()
     }
     
-    init(withPublicKey pubkey: String, testnet: Bool = false) {
-        publicKey = pubkey
+    init(withPublicKey pubkey: Data, network: BTCNetwork = .main) {
+        self.publicKey = pubkey
         let hashedPk = hash160(pubkey: pubkey)
-        let modifiedPubKey = concatenateAddress(hashedPublicKey: hashedPk, testnet: testnet)
-        address = modifiedPubKey.base58CheckEncodeHexString()
+        let modifiedPubKey = concatenateAddress(hashedPublicKey: hashedPk, network: network)
+        address = modifiedPubKey.toHexString().base58CheckEncodeHexString()
     }
     
     /**
      Performs two hash functions: sha256, then ripemd160.
      */
-    private func hash160(pubkey: String) -> String {
-        let pubKeyToHash: Data = pubkey.hexStringData()
-        let hashedPubkeyHex: String = pubKeyToHash.hashDataSHA256().toHexString()
-        let hash160: String = RIPEMD.hexStringDigest(input: hashedPubkeyHex)
+    private func hash160(pubkey: Data) -> Data {
+        let hashedPubkey = pubkey.hashDataSHA256()
+        let hash160: Data = RIPEMD.digest(input: hashedPubkey)
         return hash160
     }
     
     /**
      Concatenates the final address (version || payload || checksum).
      */
-    private func concatenateAddress(hashedPublicKey: String, testnet: Bool = false) -> String {
-        let prependedPublicKey = prependVersionByte(hashedPublicKey: hashedPublicKey, testnet: testnet)
-        let data = prependedPublicKey.hexStringData()
-        let checksum = data.SHA256ChecksumHexString()
-        let modifiedPublicKey = String(prependedPublicKey + checksum)
+    private func concatenateAddress(hashedPublicKey: Data, network: BTCNetwork = .main) -> Data {
+        let prependedPublicKey = [network.publicKeyHash].data + hashedPublicKey
+        let checksum = prependedPublicKey.doubleSHA256().prefix(4)
+        let modifiedPublicKey = prependedPublicKey + checksum
         return modifiedPublicKey
     }
     
-    /**
-     Prepends the version byte to the address according to network.
-     */
-    private func prependVersionByte(hashedPublicKey: String, testnet: Bool = false) -> String {
-        var pubkey: String
-        if testnet { pubkey = String("6f" + hashedPublicKey) }
-        else { pubkey = String("00" + hashedPublicKey) }
-        return pubkey
-    }
 }
--- HDWalletTests/BTCKeyTests.swift
+++ HDWalletTests/BTCKeyTests.swift
@@ -5,4 +5,10 @@ import XCTest
 
 class BTCKeyTests: XCTestCase {
 
+    func testGenerateAddress() {
+        let key1 = BTCKey(withPrivateKey: "11095327A9E4921336E93834D8A93395476FC3665B59668459F9AFC9A3917461".hexStringData())
+        let key2 = BTCKey(withPrivateKey: "2E04AA55FD3B8C0AD544D7940961C2E294F034E4A90BDA4A88D972E417C9DDAD".hexStringData(), network: .test)
+        XCTAssertEqual(key1.address!, "1MRVoPUVzDk8SRk9VpFEuASrcFiigxBgtR")
+        XCTAssertEqual(key2.address!, "mkP75UgGqrFaU2pD272wfCRJKzvC2iV5tg")
+    }
 }
