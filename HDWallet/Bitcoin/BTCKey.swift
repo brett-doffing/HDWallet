51129023055829eff9109c50bf94e0d38b012135
Brett Doffing <doffing81@bretts-macbook-pro.local>
2019-03-20 17:11:31 -0500

    Added another convenience initializer to get rid of redundant calculations for public key.

--- HDWallet/Bitcoin/BTCKey.swift
+++ HDWallet/Bitcoin/BTCKey.swift
@@ -2,17 +2,27 @@
 
 import Foundation
 
+// TODO: Determine if class is necessary or redundant.
 class BTCKey {
     
     var privateKey: Data?
     var publicKey: Data?
     var address: String?
     
+    // TODO: Determine if necessary or redundant.
     init(withPrivateKey prvkey: Data, network: BTCNetwork = .main) {
         self.privateKey = prvkey
         let pubkey = BTCCurve.shared.generatePublicKey(privateKey: prvkey)
         self.publicKey = pubkey
         let hashedPk = hash160(pubkey: pubkey!)
+        let modifiedPubKey = concatenateAddress(hashedPublicKey: hashedPk, network: network)
+        self.address = modifiedPubKey.toHexString().base58CheckEncodeHexString()
+    }
+    
+    init(withPrivateKey prvkey: Data, andPublicKey pubkey: Data, network: BTCNetwork = .main) {
+        self.privateKey = prvkey
+        self.publicKey = pubkey
+        let hashedPk = hash160(pubkey: pubkey)
         let modifiedPubKey = concatenateAddress(hashedPublicKey: hashedPk, network: network)
         self.address = modifiedPubKey.toHexString().base58CheckEncodeHexString()
     }
