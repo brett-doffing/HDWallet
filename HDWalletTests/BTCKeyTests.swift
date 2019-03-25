6019e9d5f643c0d210a6625f2f21e6322068b8a1
Brett Doffing <doffing81@bretts-macbook-pro.local>
2019-03-25 07:32:44 -0500

    Added Segwit address generation tests.

--- HDWalletTests/BTCKeyTests.swift
+++ HDWalletTests/BTCKeyTests.swift
@@ -12,8 +12,8 @@ class BTCKeyTests: XCTestCase {
         XCTAssertEqual(key2.address!, "mkP75UgGqrFaU2pD272wfCRJKzvC2iV5tg")
     }
     
+    // https://gist.github.com/gavinandresen/3966071
     func testP2SH() {
-        
         var privateKey1 = "5JaTXbAUmfPYZFRwrYaALK48fN6sFJp4rHqq2QSXs8ucfpE4yQU".base58CheckDecode()
         privateKey1?.removeFirst() // removes version byte
         var privateKey2 = "5Jb7fCeh1Wtm4yBBg3q3XbT6B525i17kVhy3vMC9AqfR6FH2qGk".base58CheckDecode()
@@ -42,6 +42,40 @@ class BTCKeyTests: XCTestCase {
         // UInt(0x14) = OP_CODE to push 20 bytes onto stack
         let scriptPubKey = [OP_HASH160].data + [UInt8(0x14)].data + hash + OP_EQUAL
         XCTAssertEqual(scriptPubKey.toHexString(), "a914f815b036d9bbbce5e9f2a00abd1bf3dc91e9551087")
+    }
+    
+    // https://github.com/bitcoin/bips/blob/master/bip-0049.mediawiki#test-vectors
+    func testBIP49testnet() { // This should be a keychain test, but keychains don't support testnet yet, so this is just an example of P2WPKH-nested-in-P2SH.
+        var privateKey = "cULrpoZGXiuC19Uhvykx7NugygA3k86b3hmdCeyvHYQZSxojGyXJ".base58CheckDecode()?.data
+        privateKey?.removeFirst()
+        privateKey?.removeLast()
+        XCTAssertEqual(privateKey?.toHexString(), "c9bdb49cfbaedca21c4b1f3a7803c34636b1d7dc55a717132443fc3f4c5867e8")
+        
+        let publicKey = BTCCurve.shared.generatePublicKey(privateKey: privateKey!)
+        let keyHash = publicKey?.hash160()
+        let scriptSig = [OP_0].data + [UInt8(0x14)].data + keyHash!
+        let addressBytes = scriptSig.hash160()
+        let address = ([BTCNetwork.test.scriptHash].data + addressBytes).base58CheckEncodedString
+        XCTAssertEqual(address, "2Mww8dCYPUpKHofjgcXcBCEGmniw9CoaiD2")
+    }
+    
+    // https://github.com/bitcoin/bips/blob/master/bip-0173.mediawiki#examples
+    func testBECH32() {
+        let pubkey = "0279BE667EF9DCBBAC55A06295CE870B07029BFCDB2DCE28D959F2815B16F81798".unhexlify().data
+        // For P2WPKH
+        let keyHash = pubkey.hash160()
+        // For P2WSH
+        let script = [UInt8(0x21)].data + pubkey + OP_CHECKSIG
+        let programData = script.hashDataSHA256()
+        
+        let mainP2WPKH = try? SegwitAddrCoder.init().encode(hrp: BTCNetwork.main.bech32, version: 0, program: keyHash)
+        let testP2WPKH = try? SegwitAddrCoder.init().encode(hrp: BTCNetwork.test.bech32, version: 0, program: keyHash)
+        let mainP2WSH = try? SegwitAddrCoder.init().encode(hrp: BTCNetwork.main.bech32, version: 0, program: programData)
+        let testP2WSH = try? SegwitAddrCoder.init().encode(hrp: BTCNetwork.test.bech32, version: 0, program: programData)
+        XCTAssertEqual(mainP2WPKH, "bc1qw508d6qejxtdg4y5r3zarvary0c5xw7kv8f3t4")
+        XCTAssertEqual(testP2WPKH, "tb1qw508d6qejxtdg4y5r3zarvary0c5xw7kxpjzsx")
+        XCTAssertEqual(mainP2WSH, "bc1qrp33g0q5c5txsp9arysrx4k6zdkfs4nce4xj0gdcccefvpysxf3qccfmv3")
+        XCTAssertEqual(testP2WSH, "tb1qrp33g0q5c5txsp9arysrx4k6zdkfs4nce4xj0gdcccefvpysxf3q0sl5k7")
     }
     
 }
