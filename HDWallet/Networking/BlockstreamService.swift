3d3bf16240131fc70e0be957cb6eefbfe9c1382d
Brett Doffing <doffing81@protonmail.com>
2019-04-13 18:41:46 -0500

    Rough service call to API.

--- HDWallet/Networking/BlockstreamService.swift
+++ HDWallet/Networking/BlockstreamService.swift
@@ -7,23 +7,35 @@ enum RequestType: String {
 }
 
 class BlockstreamService {
-    let baseURL: String = "https://blockstream.info/api/"
+    static let shared = BlockstreamService()
+    
+    let baseURL: String = "https://blockstream.info/testnet/api/"
+    let defaults = UserDefaults.standard
+    let isTestnet: Bool = UserDefaults.standard.bool(forKey: "testnet")
+    
+    private init() {
+        
+    }
     
     func getTransaction(withTXID txid: String) {
         let urlString = baseURL + "tx/" + txid
         guard let url = URL(string: urlString) else { return }
-        getRequest(withURL: url)
+//        getRequest(withURL: url) { (response, error) in
+//            completionHandler(response, error)
+//        }
     }
     
-    func getTransactions(forAddress address: String) {
+    func getTransactions(forAddress address: String, completionHandler: @escaping (Any?, Error?) -> ()) {
         let urlString = baseURL + "address/" + address + "/txs"
         guard let url = URL(string: urlString) else { return }
-        getRequest(withURL: url)
+        getRequest(withURL: url) { (response, error) in
+            completionHandler(response, error)
+        }
     }
     
-    private func getRequest(withURL url: URL) {
+    private func getRequest(withURL url: URL, completionHandler: @escaping (Any?, Error?) -> ()) {
         URLSession.shared.dataTask(with: url) { (data, response, error) in
-            if error != nil { print(error!.localizedDescription) }
+            if error != nil { completionHandler(nil, error) }
             
             guard let data = data else { return }
             
@@ -31,9 +43,9 @@ class BlockstreamService {
             do {
                 let jsonResponse = try JSONSerialization.jsonObject(with:data, options: .allowFragments)
                 
-                //Get back to the main queue
-                DispatchQueue.main.async { print(jsonResponse) }
-            } catch let jsonError { print(jsonError) }
+                //Get back to the main queue?
+                DispatchQueue.main.async { completionHandler(jsonResponse, nil) }
+            } catch let jsonError { completionHandler(nil, jsonError) }
         }.resume()
     }
 }
