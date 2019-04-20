025f7861147d799fc5404acbfea2b3a508920036
Brett Doffing <doffing81@protonmail.com>
2019-04-20 15:57:59 -0500

    Began LeftSidePanel implementation.

--- HDWallet/ViewContollers/LeftSidePanelVC.swift
+++ HDWallet/ViewContollers/LeftSidePanelVC.swift
@@ -4,17 +4,31 @@ import UIKit
 
 class LeftSidePanelVC: UIViewController {
     @IBOutlet weak var tableView: UITableView!
+    weak var rootContainerVC: RootContainerViewController?
     
     override func viewDidLoad() {
         super.viewDidLoad()
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        if let indexPath = self.tableView.indexPathForSelectedRow {
+            self.tableView.deselectRow(at: indexPath, animated: true)
+        }
     }
 }
 
 
 extension LeftSidePanelVC: UITableViewDelegate {
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+//        print(UIApplication.shared.keyWindow?.rootViewController?.value(forKey: "_printHierarchy"))
+        guard let rootNav = self.rootContainerVC?.rootNavigationController else { return }
         switch indexPath.row {
         case 0:
+            rootNav.viewControllers = [TabBarVC()]
+            return
+        case 1:
+            rootNav.viewControllers = [TestVC()]
             return
         default:
             return
@@ -35,14 +49,22 @@ extension LeftSidePanelVC: UITableViewDataSource {
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = UITableViewCell(style: .default, reuseIdentifier: nil)
         
-        //        switch indexPath.row {
-        //        case 0:
-        //            cell.textLabel?.text = ""
-        //        default:
-        //            return cell
-        //        }
+        cell.textLabel?.font = UIFont.bitcoinFontWith(size: 17)
+        cell.textLabel?.textColor = .white
         cell.backgroundColor = .clear
+        
+        switch indexPath.row {
+        case 0:
+            cell.textLabel?.text = "Main"
+        case 1:
+            cell.textLabel?.text = "Test"
+        default:
+            return cell
+        }
         return cell
     }
     
+    deinit {
+        print("deinitializing Side Panel VC")
+    }
 }
--- HDWallet/ViewContollers/RootContainerVC.swift
+++ HDWallet/ViewContollers/RootContainerVC.swift
@@ -6,20 +6,21 @@ import QuartzCore
 // https://www.raywenderlich.com/299-how-to-create-your-own-slide-out-navigation-panel-in-swift
 class RootContainerViewController: UIViewController {
     
-    let tabBarVC = TabBarVC()
     let rootNavigationController = RootNavigationContoller()
     let leftSidePanelVC = LeftSidePanelVC()
     var isLeftPanelExpanded = false
-    let centerPanelExpandedOffset: CGFloat = 60
+    let centerPanelExpandedOffset: CGFloat = 100
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        let tabBarVC = TabBarVC()
         self.rootNavigationController.viewControllers = [tabBarVC]
         view.addSubview(self.rootNavigationController.view)
         addChild(self.rootNavigationController)
         self.rootNavigationController.didMove(toParent: self)
         
+        self.leftSidePanelVC.rootContainerVC = self
         view.insertSubview(self.leftSidePanelVC.view, at: 0)
         addChild(self.leftSidePanelVC)
         self.leftSidePanelVC.didMove(toParent: self)
@@ -37,7 +38,7 @@ class RootContainerViewController: UIViewController {
             self.isLeftPanelExpanded = true
             self.showShadowForCenterViewController(true)
             self.animateCenterPanelXPosition(
-                targetPosition: rootNavigationController.view.frame.width - centerPanelExpandedOffset)
+                targetPosition: self.rootNavigationController.view.frame.width - self.centerPanelExpandedOffset)
             
         } else {
             animateCenterPanelXPosition(targetPosition: 0) { finished in
