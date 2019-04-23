a68e29c7179bb9bb38ff0f64ae285366ce09aa48
Brett Doffing <doffing81@protonmail.com>
2019-04-23 18:40:16 -0500

    Added expandable section for wallet types.

--- HDWallet/ViewContollers/LeftSidePanelVC.swift
+++ HDWallet/ViewContollers/LeftSidePanelVC.swift
@@ -3,7 +3,10 @@
 import UIKit
 
 class LeftSidePanelVC: UITableViewController {
+    
     weak var rootContainerVC: RootContainerViewController?
+    var walletCellExpanded = false
+    let walletCellData = ["P2PKH","P2SH (Segwit)","Bech32 (Segwit)","PayNms"]
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -22,11 +25,15 @@ class LeftSidePanelVC: UITableViewController {
     
     // MARK: Delegate
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-        //        print(UIApplication.shared.keyWindow?.rootViewController?.value(forKey: "_printHierarchy"))
+//        print(UIApplication.shared.keyWindow?.rootViewController?.value(forKey: "_printHierarchy"))
         guard let rootNav = self.rootContainerVC?.rootNavigationController else { return }
-        switch indexPath.row {
+        switch indexPath.section {
         case 0:
-            rootNav.viewControllers = [TabBarVC()]
+            if indexPath.row == 0 {
+                rootNav.viewControllers = [TabBarVC()]
+            } else if indexPath.row == 1 {
+                
+            }
         case 3:
             rootNav.viewControllers = [SettingsVC()]
         default:
@@ -37,11 +44,14 @@ class LeftSidePanelVC: UITableViewController {
     
     // MARK: Data Source
     override func numberOfSections(in tableView: UITableView) -> Int {
-        return 1
+        return 4
     }
     
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 4
+        if section == 0 && self.walletCellExpanded == true {
+            return 5
+        }
+        return 1
     }
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
@@ -52,19 +62,38 @@ class LeftSidePanelVC: UITableViewController {
         cell.backgroundColor = .clear
         cell.selectionStyle = .none
         
-        switch indexPath.row {
+        switch indexPath.section {
         case 0:
-            cell.textLabel?.text = "Wallets"
+            if indexPath.row == 0 {
+                cell.textLabel?.text = "Wallet"
+                let walletAccessory = UIButton(frame: CGRect(x: cell.frame.size.width - 100, y: 5, width: 50, height: cell.frame.size.height - 10))
+                walletAccessory.setTitle("Type", for: .normal)
+                walletAccessory.setTitleColor(.lightGray, for: .normal)
+                walletAccessory.addTarget(self, action: #selector(self.expandWallets), for: .touchUpInside)
+                cell.addSubview(walletAccessory)
+            } else {
+                cell.textLabel?.text = self.walletCellData[indexPath.row - 1]
+            }
         case 1:
             cell.textLabel?.text = "Watch-Only"
         case 2:
-            cell.textLabel?.text = "Node(s)"
+            cell.textLabel?.text = "Nodes"
         case 3:
             cell.textLabel?.text = "Settings"
         default:
             return cell
         }
         return cell
+    }
+    
+    @objc func expandWallets() {
+        if self.walletCellExpanded == true {
+            self.walletCellExpanded = false
+        } else {
+            self.walletCellExpanded = true
+        }
+        let section0 = IndexSet.init(integer: 0)
+        tableView.reloadSections(section0, with: .fade)
     }
     
     deinit {
