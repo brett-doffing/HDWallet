61a86726d12e2dd0a6182a35c4d74d01e5e67f4a
Brett <doffing81@protonmail.com>
2019-06-03 02:21:31 -0500

    Commented out deletion call.

--- HDWallet/AppDelegate.swift
+++ HDWallet/AppDelegate.swift
@@ -13,7 +13,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
         
-        deleteAllCoreData()
+//        deleteAllCoreData()
         
         // Default to testnet
         if UserDefaults.standard.value(forKey: "testnet") == nil { UserDefaults.standard.set(true, forKey: "testnet") }
