af7addf4b257d4e0e1b3fd5afd6f59b88a23ed4f
Brett <doffing81@protonmail.com>
2019-06-04 02:07:33 -0500

    Added method to post a raw transaction.

--- HDWallet/Networking/BlockstreamService.swift
+++ HDWallet/Networking/BlockstreamService.swift
@@ -26,6 +26,36 @@ class BlockstreamService {
         }
     }
     
+    func postRawTransaction(rawTX: String) {
+        let urlString = baseURL + "tx"
+        guard let url = URL(string: urlString) else { return }
+        
+        var request = URLRequest(url: url)
+        request.setValue("text/plain", forHTTPHeaderField: "Content-Type")
+        request.httpMethod = "POST"
+        request.httpBody = rawTX.data(using: .utf8)!
+
+        let task = URLSession.shared.dataTask(with: request) { data, response, error in
+            guard let data = data,
+                let response = response as? HTTPURLResponse,
+                error == nil else {                                              // check for fundamental networking error
+                    print("error", error ?? "Unknown error")
+                    return
+            }
+            
+            guard (200 ... 299) ~= response.statusCode else {                    // check for http errors
+                print("statusCode should be 2xx, but is \(response.statusCode)")
+                print("response = \(response)")
+                return
+            }
+            
+            let responseString = String(data: data, encoding: .utf8)
+            print("responseString = \(responseString)")
+            NotificationCenter.default.post(name: .transactionPosted, object: nil)
+        }
+        task.resume()
+    }
+    
     func getTransactions(forAddress address: String, completionHandler: @escaping (BlockstreamResponseObject?, Error?) -> ()) {
         let urlString = baseURL + "address/" + address + "/txs"
         guard let url = URL(string: urlString) else { return }
