1baf4b2d4e497abbda12d6d3f9bd12f087e36719
Brett <doffing81@protonmail.com>
2019-06-04 02:08:20 -0500

    Added a notification name for posting a transaction.

--- HDWallet/Extensions.swift
+++ HDWallet/Extensions.swift
@@ -5,6 +5,7 @@ import UIKit
 extension Notification.Name {
     static let scannedQRCode = Notification.Name("scannedQRCode")
     static let toggleLeftSidePanel = Notification.Name("toggleLeftSidePanel")
+    static let transactionPosted = Notification.Name("transactionPosted")
 }
 
 extension UIFont {
