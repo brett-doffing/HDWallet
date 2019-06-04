7a800fa3cb045ef529d707e742ce4c3347d97949
Brett <doffing81@protonmail.com>
2019-06-04 02:09:15 -0500

    Updated to begin working with Core Data.

--- HDWallet/ViewContollers/TransactionsVC.swift
+++ HDWallet/ViewContollers/TransactionsVC.swift
@@ -1,17 +1,52 @@
 // TransactionsVC.swift
 
 import UIKit
+import CoreData
 
-class TransactionsVC: UIViewController {
-    @IBOutlet weak var tableView: UITableView!
+class TransactionsVC: UITableViewController {
+    var addresses: [Address] = []
+    var outputs: [Vout] = []
     
-    deinit {
-        print("deinitializing Transactions VC")
+    override func viewDidLoad() {
+        
     }
-}
-
-extension TransactionsVC: UITableViewDelegate {
-    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        do {
+            addresses = try AppDelegate.viewContext.fetch(Address.fetchRequest())
+            
+            let predicate = NSPredicate(format:"scriptPubKey_address IN %@", addresses.map{$0.str})
+            let fetchOutputs: NSFetchRequest<Vout> = Vout.fetchRequest()
+            fetchOutputs.predicate = predicate
+            outputs = try AppDelegate.viewContext.fetch(fetchOutputs)
+            
+            tableView.reloadData()
+        } catch {
+            print(error)
+        }
+    }
+    
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return addresses.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = UITableViewCell(style: .subtitle, reuseIdentifier: nil)
+        
+        
+        let output = outputs[indexPath.row]
+        cell.textLabel?.text = "₿ \(output.value)"
+        cell.detailTextLabel?.text = output.scriptPubKey_address
+//        cell.backgroundColor = .clear
+        return cell
+    }
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         switch indexPath.row {
         case 0:
             return
@@ -19,29 +54,8 @@ extension TransactionsVC: UITableViewDelegate {
             return
         }
     }
-}
-
-extension TransactionsVC: UITableViewDataSource {
     
-    func numberOfSections(in tableView: UITableView) -> Int {
-        return 1
+    deinit {
+        print("deinitializing Transactions VC")
     }
-    
-    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 4
-    }
-    
-    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = UITableViewCell(style: .default, reuseIdentifier: nil)
-        
-//        switch indexPath.row {
-//        case 0:
-//            cell.textLabel?.text = ""
-//        default:
-//            return cell
-//        }
-        cell.backgroundColor = .clear
-        return cell
-    }
-    
 }
